5dae39d50df0f5208438b6d39d74f0ca884e64ea
ChrisLozano86 <chris.lozano8603@gmail.com>
2020-10-25 16:38:22 -0600

    “.gitignore”

--- template/email_templates/template_email_activation.php
+++ template/email_templates/template_email_activation.php
@@ -8,7 +8,7 @@
 			</div>
 				
 			<div id="content" style="font-size: 16px; padding: 25px; background-color: #fff;
-				moz-border-radius: 10px; -webkit-border-radius: 10px; border-radius: 10px; -khtml-border-radius: 10px;
+				-webkit-border-radius: 10px; border-radius: 10px; -khtml-border-radius: 10px;
 				border-color: #A3D0F8; border-width: 4px 1px; border-style: solid;">
 
 				<h1 style="font-size: 22px;"><center>{EMAIL_TITLE}</center></h1>
